74d8136b85fff9d30052c9df46c7faea1aeeaa3b
Cody Wilson <cody@codifynw.com>
2022-01-14 23:07:09 -0800

    some styling

--- web/js/VanillaComponents/VanillaProduct.js
+++ web/js/VanillaComponents/VanillaProduct.js
@@ -1,30 +1,30 @@
+import "../../css/vanilla/product.css";
+
 // CLASS COMPONENT
 export class VanillaProduct {
   constructor(product) {
     const productElement = document.createElement("div");
     productElement.classList.add(`product`, `${product.style.style}`);
     productElement.innerHTML = this.fillProductElement(product);
-    this.addEventListeners();
+    this.addEventListeners(productElement);
     return productElement;
   }
 
   fillProductElement(product) {
     return `
-        <div class="product-image">
-            <img src="${product.style.image}" alt="${product.name}">
+        <div class="product-image-wrap" title="${product.name}">
+            <img class="product-image" src="${product.style.image}">
         </div>
-        <div class="product-info" onClick=${this.handleClick}>
+        <div class="product-info">
             <h3>${product.name}</h3>
             <div>$${product.price}</div>
             <p>${product.description}</p>
         </div>`;
   }
 
-  handleClick() {
-    console.log("click");
-  }
-
   addEventListeners(productElement) {
-    console.log("add listened");
+    productElement.addEventListener("click", () => {
+      console.log("Clicked on product");
+    });
   }
 }
